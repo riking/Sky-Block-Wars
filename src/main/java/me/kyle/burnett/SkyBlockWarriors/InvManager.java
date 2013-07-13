ef63bfa3f4daea8832c709f77c6654d4c7f92769
riking <rikingcoding@gmail.com>
2013-07-12 17:01:30 -0700

    Remove more NMS usage: make InvManager save a list
    ConfigurationSections handle lists of ConfigurationSerializable objects
    quite nicely. No need to turn a NBT stream into base64 and save that as a
    string.

--- src/main/java/me/kyle/burnett/SkyBlockWarriors/InvManager.java
+++ src/main/java/me/kyle/burnett/SkyBlockWarriors/InvManager.java
@@ -1,39 +1,40 @@
 package me.kyle.burnett.SkyBlockWarriors;
 
+import java.util.List;
+
 import me.kyle.burnett.SkyBlockWarriors.Configs.ConfigManager;
 import me.kyle.burnett.SkyBlockWarriors.Utils.InventoryUtil;
 
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class InvManager {
-	
 	static InvManager instance = new InvManager();
-	
+
 	public InvManager getInstance(){
 		return instance;
 	}
-	
-	public void saveInv(Player p){
-		
-		Inventory main = InventoryUtil.getInstance().getContentInventory(p.getInventory());
-		Inventory armor = InventoryUtil.getInstance().getArmorInventory(p.getInventory());
-		
-		Main.getInstance().Inv.set(p.getName() +  ".Main", InventoryUtil.getInstance().toBase64(main));
-		Main.getInstance().Inv.set(p.getName() + ".Armor", InventoryUtil.getInstance().toBase64(armor));
-		
+
+	public static void saveInv(Player p){
+		List<ItemStack> main = InventoryUtil.getInstance().getContentInventory(p.getInventory());
+		List<ItemStack> armor = InventoryUtil.getInstance().getArmorInventory(p.getInventory());
+
+		Main.getInstance().Inv.set(p.getName() +  ".Main", main);
+		Main.getInstance().Inv.set(p.getName() + ".Armor", armor);
+
 		ConfigManager.getInstance().saveYamls();
 	}
-	
-	public void restoreInv(Player p){
-		
-		Inventory inv = InventoryUtil.getInstance().fromBase64(Main.getInstance().Inv.getString(p.getName() + ".Main"));
-		Inventory armor = InventoryUtil.getInstance().fromBase64(Main.getInstance().Inv.getString(p.getName() + ".Armor"));
+
+	public static void restoreInv(Player p){
+		List<?> main = Main.getInstance().Inv.getList(p.getName() + ".Main");
+		List<?> armor = Main.getInstance().Inv.getList(p.getName() + ".Armor");
 
 		p.getInventory().clear();
-		p.getInventory().setContents(inv.getContents());
-		p.getInventory().setArmorContents(armor.getContents());
-		
+		if (main != null) {
+			p.getInventory().setContents(main.toArray(new ItemStack[36]));
+		}
+		if (armor != null) {
+			p.getInventory().setArmorContents(armor.toArray(new ItemStack[4]));
+		}
 	}
-
 }
--- src/main/java/me/kyle/burnett/SkyBlockWarriors/Listeners/PlayerLeave.java
+++ src/main/java/me/kyle/burnett/SkyBlockWarriors/Listeners/PlayerLeave.java
@@ -1,46 +1,27 @@
 package me.kyle.burnett.SkyBlockWarriors.Listeners;
 
 import me.kyle.burnett.SkyBlockWarriors.GameManager;
+import me.kyle.burnett.SkyBlockWarriors.InvManager;
 import me.kyle.burnett.SkyBlockWarriors.Main;
-import me.kyle.burnett.SkyBlockWarriors.Utils.InventoryUtil;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.Inventory;
 
 public class PlayerLeave implements Listener{
-	
-	
 	@EventHandler
 	public void onLeave(PlayerQuitEvent e){
-		
 		Player p = e.getPlayer();
-		
-		if(GameManager.getInstance().isPlayerInGame(p)){
-			
-			p.teleport(Main.getInstance().getLobby());
-						
-			GameManager.getInstance().leaveGame(p);
-			
-			Inventory main = InventoryUtil.getInstance().fromBase64(Main.getInstance().Inv.getString(p.getName() + ".Main"));
-			Inventory armor = InventoryUtil.getInstance().fromBase64(Main.getInstance().Inv.getString(p.getName() + ".Armor"));
-			
-			p.getInventory().clear();
-			
-			if(main != null){
-				p.getInventory().setContents(main.getContents());
 
-			}
-			
-			if(armor != null){
-				p.getInventory().setArmorContents(armor.getContents());
-			}
-		
+		if(GameManager.getInstance().isPlayerInGame(p)){
+
+			p.teleport(Main.getInstance().getLobby());
+			GameManager.getInstance().leaveGame(p);
+			InvManager.restoreInv(p);
+
 			GameManager.getInstance().getPlayerGame(p).broadCastGame(ChatColor.GOLD +"Player " + GameManager.getInstance().getPlayerGame(p).getTeamColor(p) + p.getName() + ChatColor.GOLD + " has left the game.");
 		}
-		
 	}
 }
