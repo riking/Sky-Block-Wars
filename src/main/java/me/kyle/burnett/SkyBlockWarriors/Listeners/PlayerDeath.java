1ff185b907227026d8d2fbb7aa0e12909b52bb37
riking <rikingcoding@gmail.com>
2013-07-12 17:01:30 -0700

    Remove more NMS usage: make InvManager save a list
    ConfigurationSections handle lists of ConfigurationSerializable objects
    quite nicely. No need to turn a NBT stream into base64 and save that as a
    string.

--- src/main/java/me/kyle/burnett/SkyBlockWarriors/Listeners/PlayerDeath.java
+++ src/main/java/me/kyle/burnett/SkyBlockWarriors/Listeners/PlayerDeath.java
@@ -1,8 +1,8 @@
 package me.kyle.burnett.SkyBlockWarriors.Listeners;
 
 import me.kyle.burnett.SkyBlockWarriors.GameManager;
+import me.kyle.burnett.SkyBlockWarriors.InvManager;
 import me.kyle.burnett.SkyBlockWarriors.Main;
-import me.kyle.burnett.SkyBlockWarriors.Utils.InventoryUtil;
 import net.minecraft.server.v1_6_R2.Packet205ClientCommand;
 
 import org.bukkit.ChatColor;
@@ -12,46 +12,35 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.inventory.Inventory;
 
 public class PlayerDeath implements Listener{
-	
+
 	@EventHandler
 	public void onDeath(PlayerDeathEvent e){
-		
+
 		Entity ent = e.getEntity();
-		
+
 		if(ent instanceof Player){
-			
+
 			Player p = (Player) ent;
-			
-			
+
+
 			if(GameManager.getInstance().isPlayerInGame(p)){
-			
+
 				Packet205ClientCommand packet = new Packet205ClientCommand();
 				packet.a = 1;
-				
-				((CraftPlayer) e.getEntity()).getHandle().playerConnection.sendPacket(packet);
-				
-				p.teleport(Main.getInstance().getLobby());
-				
-				GameManager.getInstance().leaveGame(p);
-				
-				Inventory main = InventoryUtil.getInstance().fromBase64(Main.getInstance().Inv.getString(p.getName() + ".Main"));
-				Inventory armor = InventoryUtil.getInstance().fromBase64(Main.getInstance().Inv.getString(p.getName() + ".Armor"));
-				
-				if(main != null){
-					p.getInventory().setContents(main.getContents());
 
-				}
-				
-				if(armor != null){
-					p.getInventory().setArmorContents(armor.getContents());
-				}
-				
+				((CraftPlayer) e.getEntity()).getHandle().playerConnection.sendPacket(packet);
+
+				p.teleport(Main.getInstance().getLobby());
+
+				GameManager.getInstance().leaveGame(p);
+
+				InvManager.restoreInv(p);
+
 				p.sendMessage(ChatColor.RED + "You were killed by " + ChatColor.GOLD + e.getEntity().getLastDamage() + ChatColor.RED + ".");
-				
-				
+
+
 				GameManager.getInstance().getPlayerGame(p).broadCastGame(ChatColor.GOLD +"Player " + GameManager.getInstance().getPlayerGame(p).getTeamColor(p) + p.getName() + ChatColor.GOLD + " has left the game.");
 
 			}
